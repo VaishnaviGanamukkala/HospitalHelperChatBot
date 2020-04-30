73144e39dc3cae18e3778b0dff9f546440a7e488
VaishnaviGanamukkala <vyshu3120@gmail.com>
2020-04-30 21:50:53 +0530

    Adding custom action to get API search results

--- actions.py
+++ actions.py
@@ -4,24 +4,39 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
 # from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+
+import urllib.request, json
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+class ActionGetSideEffects(Action):
+     def name(self):
+         return "action_get_side_effects"
+
+     def run(self, dispatcher, tracker, domain):
+         
+         side_effects = []
+
+         medicine = tracker.get_slot('medicine')
+         url = "https://api.fda.gov/drug/event.json?search=patient.drug.medicinalproduct:\"" + medicine.upper() + "\"&limit=10"
+         print(url)
+         with urllib.request.urlopen(url) as record:
+             values = json.load(record)
+             record.close()
+         for each_res in values['results']:
+             for reaction in each_res['patient']['reaction']:
+                 side_effects.append(reaction['reactionmeddrapt'])
+         
+         say_effects = ""
+         for eff in side_effects:
+             say_effects += (eff + " ")
+         if len(side_effects) == 0:
+             say_effects += "No side effects found"
+         elif len(side_effects) == 1:
+             say_effects += ("is the side effect caused due to" + medicine[0].upper() + medicine[1:].lower())
+         else:
+             say_effects += ("are the side effects caused due to " + medicine[0].upper() + medicine[1:].lower())
+         dispatcher.utter_message(text=say_effects)
+
+         return []
