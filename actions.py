fd35f5262c22566720b7566e90a6e59a43123cef
VaishnaviGanamukkala <vyshu3120@gmail.com>
2020-05-14 19:26:41 +0530

    Adding limit on side effects

--- actions.py
+++ actions.py
@@ -7,6 +7,7 @@
 # from typing import Any, Text, Dict, List
 
 import urllib.request, json
+import operator
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
@@ -16,7 +17,7 @@ class ActionGetSideEffects(Action):
 
      def run(self, dispatcher, tracker, domain):
          
-         side_effects = []
+         side_effects = {}
 
          medicine = tracker.get_slot('medicine')
          url = "https://api.fda.gov/drug/event.json?search=patient.drug.medicinalproduct:\"" + medicine.upper() + "\"&limit=10"
@@ -26,17 +27,35 @@ class ActionGetSideEffects(Action):
              record.close()
          for each_res in values['results']:
              for reaction in each_res['patient']['reaction']:
-                 side_effects.append(reaction['reactionmeddrapt'])
+                 se = reaction['reactionmeddrapt']
+                 if se in side_effects:
+                     side_effects[se] += 1
+                 else:
+                     side_effects[se] = 1
          
+         
+         sorted_effects = sorted(side_effects.items(), key = operator.itemgetter(1), reverse = True)
          say_effects = ""
-         for eff in side_effects:
-             say_effects += (eff + " ")
-         if len(side_effects) == 0:
-             say_effects += "No side effects found"
-         elif len(side_effects) == 1:
-             say_effects += ("is the side effect caused due to" + medicine[0].upper() + medicine[1:].lower())
-         else:
+         count = len(sorted_effects)
+         if count > 7:
+             i = 1
+             for eff in sorted_effects:
+                 say_effects += (eff[0] + ", ")
+                 if i == 7:
+                     break
+                 i += 1
              say_effects += ("are the side effects caused due to " + medicine[0].upper() + medicine[1:].lower())
+         else:
+             if count == 0:
+                 say_effects += "No side effects found. Please check if you gave the right spelling of the medicine."
+             else:
+                 for eff in sorted_effects:
+                    say_effects += (eff[0] + ", ")
+                 if count == 1:
+                     say_effects += ("is the side effect caused due to" + medicine[0].upper() + medicine[1:].lower())
+                 else:
+                     say_effects += ("are the side effects caused due to " + medicine[0].upper() + medicine[1:].lower())
+
          dispatcher.utter_message(text=say_effects)
 
          return []
