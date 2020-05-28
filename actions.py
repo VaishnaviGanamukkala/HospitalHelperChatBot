205d71ebdb3b96cb57530e5fb1bcedfd9ca25d69
VaishnaviGanamukkala <vyshu3120@gmail.com>
2020-05-28 14:46:26 +0530

    Adding the feauture of symtoms analyser

--- actions.py
+++ actions.py
@@ -10,6 +10,7 @@ import urllib.request, json
 import operator
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.forms import FormAction
 
 class ActionGetSideEffects(Action):
      def name(self):
@@ -59,3 +60,87 @@ class ActionGetSideEffects(Action):
          dispatcher.utter_message(text=say_effects)
 
          return []
+
+
+class ActionGetDiagnosisAndSpecializations(Action):
+     def name(self):
+         return "action_get_diagnosis_and_specializations"
+
+     def run(self, dispatcher, tracker, domain):
+         auth_token = "your_api_key"
+
+         with open('Symptoms_Sandbox.json') as f:
+             data = json.load(f)
+             f.close()
+         symptoms_id_map = {}
+         for each in data:
+             symptoms_id_map[each['Name'].lower()] = each['ID']
+         url = "https://sandbox-healthservice.priaid.ch/diagnosis?symptoms=["
+         symptoms = tracker.get_slot('symptoms')
+         gender = tracker.get_slot('gender')
+         yob = tracker.get_slot('year_of_birth')
+
+         symptoms_ids = []
+         check= set([])
+         for each in symptoms:
+             each = each.lower()
+             if each in symptoms_id_map:
+                 if symptoms_id_map[each] not in check:
+                    symptoms_ids.append(symptoms_id_map[each])
+                    check.add(symptoms_id_map[each])
+         print(symptoms_ids, gender, yob)
+         for idx, sid in enumerate(symptoms_ids):
+             url += str(sid)
+             if idx != len(symptoms_ids)-1:
+                 url += ","
+         url += "]&gender="
+         if type(gender) == list:
+            url += gender[0]
+         else:
+             url += gender
+         url += "&year_of_birth="
+         url += yob[0]
+         url += "&token="
+         url += auth_token
+         url += "&format=json&language=en-gb"
+
+         print(url)
+         with urllib.request.urlopen(url) as record:
+             results = json.load(record)
+             record.close()
+
+         say_diseases = ""
+         for each in results:
+             say_diseases += "You may have " + each['Issue']['Name'] + " with a predicted accuracy of " + str(each['Issue']['Accuracy']) + ". "
+             if len(each['Specialisation']) != 0:
+                 say_diseases += "Please visit "
+                 for idx, specialist in enumerate(each['Specialisation']):
+                     say_diseases += specialist['Name']
+                     if idx != len(each['Specialisation']) - 1:
+                         say_diseases += ", "
+                 
+                 if len(each['Specialisation']) == 1:
+                     say_diseases += " department. "
+                 else:
+                     say_diseases += " departments. "
+
+         if say_diseases == "":
+             say_diseases += "No possible diseases found!"
+
+         dispatcher.utter_message(text=say_diseases)
+
+         return []
+
+
+class SymptomsForm(FormAction):
+
+    def name(self):
+        return "symptoms_analysis_form"
+
+    @staticmethod
+    def required_slots(tracker):
+        return ["symptoms", "gender", "year_of_birth"]
+
+    def submit(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(template="utter_submit")
+        return []
